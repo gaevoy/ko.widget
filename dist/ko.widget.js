b8a6c98cc3fc12606565c72838ab7c1e3ddc8ab3
Vladimir Gaevoy <vova.gaevoy@gmail.com>
2016-01-28 15:31:39 +0200

    Clean obsolete functionality

--- dist/ko.widget.js
+++ dist/ko.widget.js
@@ -13,15 +13,11 @@ function Widget(viewModel, view) {
     var self = this;
     var element = null;
 
-    this.setTemplate = function (name, template) {
-        var re = new RegExp("(<!--" + name + "Template-->[ \r\n]*)((.|\r|\n)*?)([ \r\n]*<!--/" + name + "Template-->)", "g");
-        view = view.replace(re, "$1" + template + "$4");
-    };
     this.exportMethods = function () {
         var methodNames = arguments;
         for (var i = 0; i < methodNames.length; i++) {
             var methodName = methodNames[i];
-            if (viewModel != undefined && viewModel[methodName] !== undefined) {
+            if (viewModel && viewModel[methodName]) {
                 self[methodName] = viewModel[methodName];
             }
         }
@@ -31,12 +27,10 @@ function Widget(viewModel, view) {
             return viewModel.init.apply(viewModel, arguments);
     };
     this.appendTo = function (elementToAppend) {
-        element = elementToAppend;
+        element = $(elementToAppend);
         var viewElement = $(view);
         element.append(viewElement);
-        if (viewModel == null) {
-            throw new Error("Widget can not be attached because of viewModel is null");
-        }
+        if (viewModel == null) throw new Error("Widget can not be attached because of viewModel is null");
         ko.applyBindings(viewModel, viewElement[0]);
         if (viewModel.bound) viewModel.bound();
     };
@@ -48,26 +42,9 @@ function Widget(viewModel, view) {
             element = null;
         }
     };
-    this.settings = function (object) {
-        if (!!!object) return;
-        for (var key in object) {
-            if (object.hasOwnProperty(key)) {
-                if (/.*\.settings$/.test(key)) {
-                    var field = viewModel[key.replace(".settings", "")];
-                    field().settings(object[key]);
-                } else {
-                    var field = viewModel[key];
-                    if (field) {
-                        field(object[key]);
-                    }
-                }
-            }
-        }
-    };
 };
 
 Widget.extend = function (derivedInstance, args) {
-    args = (args === undefined) ? [] : args;
     Widget.apply(derivedInstance, args);
 };
 var widgetForDomDataKey = "__ko_widget__";
--- src/Widget.js
+++ src/Widget.js
@@ -2,15 +2,11 @@
     var self = this;
     var element = null;
 
-    this.setTemplate = function (name, template) {
-        var re = new RegExp("(<!--" + name + "Template-->[ \r\n]*)((.|\r|\n)*?)([ \r\n]*<!--/" + name + "Template-->)", "g");
-        view = view.replace(re, "$1" + template + "$4");
-    };
     this.exportMethods = function () {
         var methodNames = arguments;
         for (var i = 0; i < methodNames.length; i++) {
             var methodName = methodNames[i];
-            if (viewModel != undefined && viewModel[methodName] !== undefined) {
+            if (viewModel && viewModel[methodName]) {
                 self[methodName] = viewModel[methodName];
             }
         }
@@ -20,12 +16,10 @@
             return viewModel.init.apply(viewModel, arguments);
     };
     this.appendTo = function (elementToAppend) {
-        element = elementToAppend;
+        element = $(elementToAppend);
         var viewElement = $(view);
         element.append(viewElement);
-        if (viewModel == null) {
-            throw new Error("Widget can not be attached because of viewModel is null");
-        }
+        if (viewModel == null) throw new Error("Widget can not be attached because of viewModel is null");
         ko.applyBindings(viewModel, viewElement[0]);
         if (viewModel.bound) viewModel.bound();
     };
@@ -37,25 +31,8 @@
             element = null;
         }
     };
-    this.settings = function (object) {
-        if (!!!object) return;
-        for (var key in object) {
-            if (object.hasOwnProperty(key)) {
-                if (/.*\.settings$/.test(key)) {
-                    var field = viewModel[key.replace(".settings", "")];
-                    field().settings(object[key]);
-                } else {
-                    var field = viewModel[key];
-                    if (field) {
-                        field(object[key]);
-                    }
-                }
-            }
-        }
-    };
 };
 
 Widget.extend = function (derivedInstance, args) {
-    args = (args === undefined) ? [] : args;
     Widget.apply(derivedInstance, args);
 };
